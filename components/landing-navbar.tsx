fe81472cbb737b79541dac59af23e9cce812e951
Siddharth Ramachandran <sidd.r2003@gmail.com>
2023-07-31 23:42:12 +0530

    update title bug

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -20,7 +20,7 @@ export const LandingNavbar = () => {
           <Image fill alt="Logo" src="/icon.png.png" />
         </div>
         <h1 className={cn("text-2xl font-bold text-white", font.className)}>
-          Genius
+          CogniNex
         </h1>
       </Link>
       <div className="flex items-center gap-x-2 z-[1000]">
